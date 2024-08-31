4ded3693b75893035be2fd2e8d696dcbea9c0593
Pavitra B Kochalapur <141141163+Pavitra-B-Kochalapur@users.noreply.github.com>
2024-08-31 19:37:22 +0530

    Add files via upload

---
+++ JDBC/Mphasis_JDBC/src/test/java/com/springboot/mphasis_jdbc/MphasisJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mphasis_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MphasisJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
